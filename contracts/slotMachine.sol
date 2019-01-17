c2513e6a99b12619e43bbb1af8f66695c1726c92
mettinger <jettinger35@gmail.com>
2019-01-17 08:27:37 -0500

    message

--- contracts/slotMachine.sol
+++ contracts/slotMachine.sol
@@ -4,6 +4,8 @@ contract SlotMachine {
 
   struct Reel {
     uint probDenominator;
+    //  THESE CUTOFFS CORRESPOND TO THE PROBABILITIES OF THE EVENTS
+    //  WHEN WE GENERATE A RANDOM NUMBER IN [0, probDenominator - 1]
     uint[] probCutoffs;
   }
 
@@ -11,6 +13,11 @@ contract SlotMachine {
     address user;
     uint amount;
     uint block;
+  }
+
+  struct HousePercentage {
+    uint numerator;
+    uint denominator;
   }
 
   address payable public owner;
@@ -21,10 +28,24 @@ contract SlotMachine {
   uint[numReel] thisOutcome;
   mapping(uint => bool) public spun;
 
+  uint constant maxHouseMembers = 20;
+  mapping(address => HousePercentage) public housePercentages;
+  mapping(address => uint) public houseAccounts;
+  address payable [maxHouseMembers] public houseMemberArray;
+
+  uint constant minPercentageIncrease = 20;
+
+  // 0 for debugging, 3 for production
+  uint constant blockDelay = 0;
+
+  // ******************************
   // DEFINE THE MACHINE
+
   uint8 constant numReel = 2;
   uint[numReel] public numSymbols;
   Reel[numReel] public machine;
+  uint public maxBetBalanceDivisor = 10;
+  uint public minBet = 1;
 
   function paytable(uint[numReel] memory outcome, uint betAmount) internal pure returns (uint) {
     if (outcome[0] == 0 && outcome[1] == 1) {
@@ -55,9 +76,11 @@ contract SlotMachine {
     }
   }
 
-  // DEFINE EVENTS
-  event BetPlaced(address user, uint amount, uint block, uint counter);
   event Spin(uint id, uint real0, uint real1, uint award);
+
+  // ***************************************
+
+  event BetPlaced(address user, uint amount, uint block, uint counter);
 
 
   constructor () public payable {
@@ -66,10 +89,11 @@ contract SlotMachine {
   }
 
   function wager () payable public {
-    require(msg.value > 0);
-    counter++;
-    bets[counter] = Bet(msg.sender, msg.value, block.number + 3);
+    require(msg.value >= minBet);
+    require(msg.value <= address(this).balance/maxBetBalanceDivisor);
+    bets[counter] = Bet(msg.sender, msg.value, block.number + blockDelay);
     emit BetPlaced(msg.sender, msg.value, block.number + 3, counter);
+    counter++;
   }
 
   function random(uint modulus) internal returns (uint) {
@@ -78,11 +102,12 @@ contract SlotMachine {
     return randomnumber;
   }
 
-  function outcomeGet(uint id) public {
+  function outcomeDetermine(uint id) public {
     uint thisRandom;
 
     for (uint i = 0; i < numReel; i++) {
       thisRandom = random(machine[i].probDenominator);
+      // CONVERT RANDOM NUMBER TO EVENT
       for (uint j = 0; j < machine[i].probCutoffs.length; j++){
         if (thisRandom <= machine[i].probCutoffs[j]) {
           thisOutcome[i] = j;
@@ -100,23 +125,82 @@ contract SlotMachine {
       require(block.number <= bet.block + 255); // make this into a refund
       require(!spun[id]);
 
-      outcomeGet(id);
+      outcomeDetermine(id);
 
       spun[id] = true;
       uint award = paytable(thisOutcome, bet.amount);
-      msg.sender.transfer(award);
+      if (award > 0) {
+        msg.sender.transfer(award);
+      }
 
+      // CUSTOMIZE THIS EVENT FOR THE MACHINE STRUCTURE
       emit Spin(id, thisOutcome[0], thisOutcome[1], award);
   }
 
-  // make co-owners
-  function fund () public payable {}
+  // GETTERS FOR DEBUGGING
+  function outcomeGet(uint id) public view returns (uint[numReel] memory) {
+    return outcomes[id];
+  }
 
-  // put limits on houseWithdraw
-  function houseWithdraw (uint withdrawAmount) public {
-    require(msg.sender == owner);
-    require(withdrawAmount < address(this).balance);
-    owner.transfer(withdrawAmount);
+  function addMember(address payable addressToAdd, uint initialFund) public {
+
+    uint currentMin = houseAccounts[houseMemberArray[0]];
+    uint currentMinIndex = 0;
+    address payable thisAddress;
+
+    for (uint i = 0; i < maxHouseMembers; i++){
+      thisAddress = houseMemberArray[i];
+      if (houseAccounts[thisAddress] == 0) {
+          houseRemoveMember(thisAddress);
+          houseMemberArray[i] = addressToAdd;
+          houseAccounts[thisAddress] = initialFund;
+          calculateHousePercentages();
+          return;
+      }
+      else {
+        if (houseAccounts[houseMemberArray[i]] < currentMin) {
+          currentMin = houseAccounts[houseMemberArray[i]];
+          currentMinIndex = i;
+        }
+      }
+    }
+    if ( initialFund > currentMin + ((currentMin * minPercentageIncrease)/100) ) {
+      thisAddress = houseMemberArray[currentMinIndex];
+      houseRemoveMember(thisAddress);
+      houseMemberArray[currentMinIndex] = addressToAdd;
+      houseAccounts[thisAddress] = initialFund;
+      calculateHousePercentages();
+      return;
+    }
+    require(0 == 1);
+  }
+
+  // make co-owners
+  function fund () public payable {
+    addMember(msg.sender, msg.value);
+    calculateHousePercentages();
+  }
+
+  // RECALULATE ALL OWNERSHIP PERCENTAGES
+  function calculateHousePercentages() public {
+    for (uint i = 0; i < maxHouseMembers; i++) {
+      housePercentages[houseMemberArray[i]].numerator = houseAccounts[houseMemberArray[i]];
+      housePercentages[houseMemberArray[i]].denominator = address(this).balance;
+    }
+  }
+
+  function houseRemoveMember(address payable houseMember) public {
+    if (houseAccounts[houseMember] > 0) {
+      houseMember.transfer(houseAccounts[houseMember]);
+      houseAccounts[houseMember] = 0;
+    }
+  }
+
+  // WITHDRAW
+  function houseWithdraw (address payable houseMember) public {
+    require(msg.sender == houseMember || msg.sender == owner);
+    houseRemoveMember(houseMember);
+    calculateHousePercentages();
   }
 
   function getBalance () view public returns (uint) {
